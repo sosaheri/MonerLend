2b318d298293dfc539b5066024bf8a544b9b3c76
Heriberto Sosa <sosaheriberto2001@gmail.com>
2019-08-03 00:25:55 -0430

    setting app

---
+++ ml/resources/views/index.blade.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Moner Lend</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('web') }}/img/core-img/favicon.ico"  >
+
+    <!-- Core Stylesheet -->
+    <link href="{{ asset('web') }}/style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="{{ asset('web') }}/css/responsive.css  " rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="colorlib-load"></div>
+    </div>
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header_area animated">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <!-- Menu Area Start -->
+                <div class="col-12 col-lg-10">
+                    <div class="menu_area">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <!-- Logo -->
+                            <a class="navbar-brand" href="#"></a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ca-navbar" aria-controls="ca-navbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                            <!-- Menu Area -->
+                            <div class="collapse navbar-collapse" id="ca-navbar">
+                                <ul class="navbar-nav ml-auto" id="nav">
+                                   
+                                </ul>
+                                <div class="sing-up-button d-lg-none">
+                                    <a href="#">Ingresa</a>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+                <!-- Signup btn -->
+                <div class="col-12 col-lg-2">
+                    <div class="sing-up-button d-none d-lg-block">
+                        <a href="#">Ingresa</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Wellcome Area Start ***** -->
+    <section class="wellcome_area clearfix" id="home">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12 col-md">
+                    <div class="wellcome-heading">
+                        <h2>MonerLend</h2>
+                        <h3>Ml</h3>
+                        <p>Ahorra e invierte inteligentemente</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- Welcome thumb -->
+        <div class="welcome-thumb wow fadeInDown" data-wow-delay="0.5s">
+            <img src="{{ asset('web') }}/img/bg-img/bits.png" alt="">
+        </div>
+    </section>
+    <!-- ***** Wellcome Area End ***** -->
+
+    <!-- ***** Special Area Start ***** -->
+    <section class="special-area bg-white section_padding_100" id="about">
+         <!-- Special Description Area -->
+         <div class="special_description_area mt-150">
+                <div class="container">
+                    <div class="row">
+                            <div class="col-lg-6">
+                                    <div class="special_description_img">
+                                        <img src="{{ asset('web') }}/img/quienesSomos.png" alt="">
+                                    </div>
+                                </div>
+
+                            <div class="col-lg-6 col-xl-5 ml-xl-auto">
+                                    <div class="special_description_content">
+                                        <h2>Quienes Somos</h2>
+                                        <p>Somos un proyecto que usa la tecnología Fintech y la revolución de la blockchain, para generar beneficios a toda la comunidad cripto. Te facilitamos financiar tus proyectos personales gracias a la facilidad para adquirir un préstamo. También puedes programarte para hacer un plan de ahorro mensual y así conseguir tus objetivos y lograr tus metas.</p>
+                                        
+                                    </div>
+                                </div>
+    
+
+    
+                    </div>
+                </div>
+            </div>
+ 
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Section Heading Area -->
+                    <div class="section-heading text-center">
+                        <h2>Ventajas de unirte a nuestra comunidad</h2>
+                        <div class="line-shape"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-4">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="single-icon">
+                            <i class="ti-mobile" aria-hidden="true"></i>
+                        </div>
+                        <h4>Fácil de usar</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit per egestas imperdiet cum, lacus platea nostra faucibus velit ultrices eros diam blandit vivamus.</p>
+                    </div>
+                </div>
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-4">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="single-icon">
+                            <i class="ti-settings" aria-hidden="true"></i>
+                        </div>
+                        <h4>Control Total</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit per egestas imperdiet cum, lacus platea nostra faucibus velit ultrices eros diam blandit vivamus.</p>
+                    </div>
+                </div>
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-4">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="single-icon">
+                            <i class="ti-lock" aria-hidden="true"></i>
+                        </div>
+                        <h4>Seguro</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit per egestas imperdiet cum, lacus platea nostra faucibus velit ultrices eros diam blandit vivamus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- ***** Special Area End ***** -->
+
+  
+
+    <!-- ***** Our Team Area Start ***** -->
+    <section class="our-Team-area bg-white  clearfix" id="team">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <!-- Heading Text  -->
+                    <div class="section-heading">
+                        <h2>Nuestros Mejores Esponsor</h2>
+                        <div class="line-shape"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="single-team-member">
+                        <div class="member-image">
+                            <img src="{{ asset('web') }}/img/team-img/team-1.jpg" alt="">
+                            <div class="team-hover-effects">
+                                <div class="team-social-icon">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                                    <a href="#"> <i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="member-text">
+                            <h4>Jackson Nash</h4>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="single-team-member">
+                        <div class="member-image">
+                            <img src="{{ asset('web') }}/img/team-img/team-2.jpg" alt="">
+                            <div class="team-hover-effects">
+                                <div class="team-social-icon">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                                    <a href="#"> <i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="member-text">
+                            <h4>Alex Manning</h4>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="single-team-member">
+                        <div class="member-image">
+                            <img src="{{ asset('web') }}/img/team-img/team-3.jpg" alt="">
+                            <div class="team-hover-effects">
+                                <div class="team-social-icon">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                                    <a href="#"> <i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="member-text">
+                            <h4>Ollie Schneider</h4>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="single-team-member">
+                        <div class="member-image">
+                            <img src="{{ asset('web') }}/img/team-img/team-4.jpg" alt="">
+                            <div class="team-hover-effects">
+                                <div class="team-social-icon">
+                                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                                    <a href="#"> <i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="member-text">
+                            <h4>Roger West</h4>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Our Team Area End ***** -->
+
+    <!-- ***** CTA Area Start ***** -->
+    <section class="our-monthly-membership section_padding_50 clearfix">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-8">
+                    <div class="membership-description">
+                        <h2>Unete a Nuestra Comunidad</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit per egestas.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="get-started-button wow bounceInDown" data-wow-delay="0.5s">
+                        <a href="#">Participa</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** CTA Area End ***** -->
+
+
+
+   
+
+    <!-- ***** Footer Area Start ***** -->
+    <footer class="footer-social-icon text-center section_padding_70 clearfix">
+        <!-- footer logo -->
+        <div class="footer-text">
+            <h2>MonerLend</h2>
+        </div>
+        <!-- social icon-->
+        <div class="footer-social-icon">
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="#"> <i class="fa fa-instagram" aria-hidden="true"></i></a>
+
+        </div>
+        <div class="footer-menu">
+            <nav>
+                <ul>
+                    
+                    <li><a href="#">Terminos &amp; Condiciones</a></li>
+                    <li><a href="#">Politica de Privacidad</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
+        <!-- Foooter Text-->
+        <div class="copyright-text">
+            <!-- ***** Removing this text is now allowed! This template is licensed under CC BY 3.0 ***** -->
+            <p>Copyright ©2019 - MonerLend</p>
+            </p> Designed & Coded with ♥ by <a href="https://larepaweb.com.ve" target="_blank">Arepa Web Group</a></p>
+        </div>
+    </footer>
+    <!-- ***** Footer Area Start ***** -->
+
+    <!-- Jquery-2.2.4 JS -->
+    <script src="{{ asset('web') }}/js/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="{{ asset('web') }}/js/popper.min.js"></script>
+    <!-- Bootstrap-4 Beta JS -->
+    <script src="{{ asset('web') }}/js/bootstrap.min.js"></script>
+    <!-- All Plugins JS -->
+    <script src="{{ asset('web') }}/js/plugins.js"></script>
+    <!-- Slick Slider Js-->
+    <script src="{{ asset('web') }}/js/slick.min.js"></script>
+    <!-- Footer Reveal JS -->
+    <script src="{{ asset('web') }}/js/footer-reveal.min.js"></script>
+    <!-- Active JS -->
+    <script src="{{ asset('web') }}/js/active.js"></script>
+</body>
+
+</html>
